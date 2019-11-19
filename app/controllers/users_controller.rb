52ab5098b46527bc5c8b3a7bf11f2b20de1c5d84
和栗　諒人 <>
2019-11-20 01:07:33 +0900

    "Finish user signup"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,11 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-  def index 
-
-  end
-
   def create
-  	@user = User.new(params[:user])
+  	@user = User.new(user_params)
   	if @user.save
-      # 保存の成功をここで扱う。
+     flash[:success] = "Welcome to the Sample App!"
+     redirect_to @user
     else
       render 'new'
     end
