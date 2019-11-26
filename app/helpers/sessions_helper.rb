6248facf34e0d1e7d6e304f634a51a4fff261319
和栗　諒人 <>
2019-11-26 13:23:38 +0900

    "Finish user edit, update, index, and destroy actions"

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
  
  def log_in(user)
-  session[:user_id] = user.user_id
+  session[:user_id] = user.id
  end
 
  def remember(user)
@@ -9,6 +9,11 @@ module SessionsHelper
   cookies.permanent.signed[:user_id] = user.id
   cookies.permanent[:remember_token] = user.remember_token
  end
+
+ def current_user?(user)
+  user == current_user
+ end
+ 
 
  def current_user
   if(user_id = session[:user_id])
@@ -38,5 +43,14 @@ module SessionsHelper
   forget(current_user)
   session.delete(:user_id)
   @current_user = nil
+ end
+
+ def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+ end
+
+ def store_location
+    session[:forwarding_url] = request.original_url if request.get?
  end
 end
