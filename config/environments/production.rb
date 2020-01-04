5f9c1cd754f22c8f43de0b8f239b2890e516e2d7
和栗　諒人 <>
2020-01-05 01:29:52 +0900

    11sho

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'shrouded-chamber-02304.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'san3373@icloud.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['sgf9inxp@kke.com'],
+    :password       => ENV['0373masato'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
